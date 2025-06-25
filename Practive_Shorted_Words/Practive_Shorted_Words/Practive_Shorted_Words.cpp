cda68d6827967c2329887d3c98a9f5dc73652769
said <saidacostacepeda03@gmail.com>
2025-06-24 23:42:24 -0500

    feat: Added a new practice which is called Shortest Word

---
+++ Practive_Shorted_Words/Practive_Shorted_Words/Practive_Shorted_Words.cpp
@@ -0,0 +1,14 @@
+﻿// Practive_Shorted_Words.cpp: define el punto de entrada de la aplicación.
+//
+
+#include "Practive_Shorted_Words.h"
+#include <string.h>
+#include <iostream>
+
+
+int main()
+{
+	std::string value = "let's go";
+	std::cout << "Hello CMake and the len is: " << value.length() << std::endl;
+	return 0;
+}
---
+++ Practive_Shorted_Words/Practive_Shorted_Words/Practive_Shorted_Words.h
@@ -0,0 +1,8 @@
+﻿// Practive_Shorted_Words.h: archivo de inclusión para archivos de inclusión estándar del sistema,
+// o archivos de inclusión específicos de un proyecto.
+
+#pragma once
+
+#include <iostream>
+
+// TODO: Haga referencia aquí a los encabezados adicionales que el programa requiere.
