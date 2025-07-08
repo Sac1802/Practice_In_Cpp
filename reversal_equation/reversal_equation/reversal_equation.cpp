0b27a8eb127e23cbaab878220de992c44cf6fdc0
said <saidacostacepeda03@gmail.com>
2025-07-08 00:33:58 -0500

    feat: add a new soluction kata: in progress

---
+++ reversal_equation/reversal_equation/reversal_equation.cpp
@@ -0,0 +1,36 @@
+﻿// reversal_equation.cpp: define el punto de entrada de la aplicación.
+//
+
+#include "reversal_equation.h"
+
+std::string reversal_equation::solve(std::string eq) {
+	std::string value_return;
+	int len = eq.length();
+	std:: string value_int;
+	int index = 0;
+	for (int i = eq.length() - 1; i >= 0; i--)
+	{
+		char c = eq[i];
+		if (isdigit(eq[i])) {
+			value_int = c + value_int;
+		}
+		else {
+			value_int = '|';
+		}
+		value_return += c;
+	}
+	for (int i = 0; i < value_return.length(); i++) {
+		if (isdigit(value_return[i])) {
+			value_return[i] = value_int[index++];
+		}
+	}
+
+	return value_return;
+}
+
+int main()
+{
+	reversal_equation reversal;
+	std::cout << reversal.solve("100*b/y") << std::endl;
+	return 0;
+}
---
+++ reversal_equation/reversal_equation/reversal_equation.h
@@ -0,0 +1,15 @@
+﻿// reversal_equation.h: archivo de inclusión para archivos de inclusión estándar del sistema,
+// o archivos de inclusión específicos de un proyecto.
+
+#pragma once
+
+#include <iostream>
+#include <string>
+#include <cctype>
+#include <algorithm>
+#include <vector>
+
+class reversal_equation {
+public:
+	std::string solve(std::string eq);
+};
